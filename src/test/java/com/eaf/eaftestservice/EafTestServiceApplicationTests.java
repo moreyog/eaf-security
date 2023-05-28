a64254801e9fbbd2b4ccd861f564fe04fbcbbae8
Yogesh More <moreyog@gmail.com>
2023-05-28 14:13:32 +0530

    Initial commit

---
+++ src/test/java/com/eaf/eaftestservice/EafTestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eaf.eaftestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EafTestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
